0562ffc1de7640a586595ef950f2d361e5115fd4
matyushinleonid <matyushinleonid@gmail.com>
2021-04-01 19:32:38 +0000

    add domains to nguyen dataset

--- src/roboscientist/datasets/read_dataset.py
+++ src/roboscientist/datasets/read_dataset.py
@@ -6,12 +6,13 @@ from pathlib import Path
 import pandas as pd
 
 
-def _str_to_equation(s):
-    expr = sp.parse_expr(s)
-    return Equation(expr)
+def _str_to_equation(row):
+    formula, domain_0, domain_1 = row
+    expr = sp.parse_expr(formula)
+    return Equation(expr, space=[[domain_0, domain_1]])
 
 def read_dataset(path):
-    df_formulae = pd.read_csv(Path(path) / 'formulae.csv', header=None, index_col=0, names=['index', 'formula'])
-    equations = df_formulae['formula'].apply(_str_to_equation).tolist()
+    df_formulae = pd.read_csv(Path(path) / 'formulae.csv', header=None, index_col=0, names=['index', 'formula', 'domain_0', 'domain_1'])
+    equations = df_formulae.apply(_str_to_equation, axis=1).tolist()
     
     return Dataset(equations)
