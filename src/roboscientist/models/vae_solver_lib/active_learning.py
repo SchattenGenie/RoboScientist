519c33a05cfe6165a09da364c7bfad344a999b06
Julia <nuwanda57@yandex.ru>
2021-04-22 03:16:06 +0300

    add max entropy picking

--- src/roboscientist/models/vae_solver_lib/active_learning.py
+++ src/roboscientist/models/vae_solver_lib/active_learning.py
@@ -1,9 +1,9 @@
 from . import optimize_constants, formula_infix_utils
-from roboscientist.datasets import equations_generation, Dataset, equations_utils, equations_base
+from roboscientist.datasets import equations_utils, equations_base
 
 import torch
 import numpy as np
-import scipy
+from scipy import special
 
 
 def empirical_entropy(X):
@@ -23,7 +23,7 @@ def empirical_entropy(X):
     R_i = (X - X[ind[:, 1]]).pow(2).sum(1).sqrt()
     # number of features
     d = X.shape[1]
-    V = np.pi ** (d / 2) / scipy.special.gamma(d / 2 + 1)
+    V = np.pi ** (d / 2) / special.gamma(d / 2 + 1)
     entropy = (len(R_i) * (R_i.pow(d))).log() + np.log(V) + 0.577
     return entropy
 
@@ -59,6 +59,29 @@ def _pick_next_point_max_var(solver, candidate_xs):
     return candidate_xs[np.argmax(var)]
 
 
+def _pick_next_point_max_entropy(solver, candidate_xs):
+    cond_x, cond_y = solver._get_condition(solver.params.active_learning_n_sample)
+    solver.model.sample(solver.params.active_learning_n_sample, solver.params.max_formula_length,
+                        solver.params.active_learning_file_to_sample, Xs=cond_x, ys=cond_y, unique=False)
+    ys = []
+    with open(solver.params.active_learning_file_to_sample) as f:
+        for line in f:
+            try:
+                f_to_eval = formula_infix_utils.clear_redundant_operations(line.strip().split(),
+                                                                           solver.params.functions,
+                                                                           solver.params.arities)
+                f_to_eval = [float(x) if x in solver.params.float_constants else x for x in f_to_eval]
+                f_to_eval = equations_utils.infix_to_expr(f_to_eval)
+                f_to_eval = equations_base.Equation(f_to_eval)
+                constants = optimize_constants.optimize_constants(f_to_eval, solver.xs, solver.ys)
+                y = f_to_eval.func(candidate_xs.reshape(-1, 1), constants)
+                ys.append(y)
+            except:
+                continue
+    entropy = torch.from_numpy(np.array(ys))
+    return candidate_xs[np.argmax(entropy)]
+
+
 def _pick_next_point_random(solver, candidate_xs):
     return candidate_xs[np.random.randint(0, len(candidate_xs), 1)]
 
@@ -69,5 +92,13 @@ def pick_next_point(solver):
         return _pick_next_point_max_var(solver, candidate_xs)
     if solver.params.active_learning_strategy == 'random':
         return _pick_next_point_random(solver, candidate_xs)
+    if solver.params.active_learning_strategy == 'entropy':
+        return _pick_next_point_max_entropy(solver, candidate_xs)
     else:
         raise 57
+
+
+if __name__ == '__main__':
+    y = torch.randn(30, 1)
+    print("Empirical entropy: {}".format(empirical_entropy(y).mean()))
+    print("Theoretical entropy: {}".format(1 / 2 + torch.tensor(3.14 * 2).sqrt().log()))
